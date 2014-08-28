6de42b65a138c2e54735dc11780a5bc0d16d5943
Amber Agard <amber.n.agard@gmail.com>
2014-08-28 14:44:21 -0500

    changed app.js

--- app.js
+++ app.js
@@ -1,42 +1,12 @@
-'use strict';
+var http = require('http');
+var connect = require('connect');
+var directory = 'public';
+var port = process.env.PORT;
 
-var dbname = process.env.DBNAME;
-var port = process.env.PORT || 4000;
+var app = connect()
+  .use(connect.logger(':remote-addr -> :method :url [:status]'))
+  .use(connect.static(directory));
 
-var traceur        = require('traceur');
-var express        = require('express');
-var less           = require('express-less');
-var morgan         = require('morgan');
-var bodyParser     = require('body-parser');
-var methodOverride = require('method-override');
-var cookieSession  = require('cookie-session');
-var initMongo      = traceur.require(__dirname + '/lib/init-mongo.js');
-var initRoutes     = traceur.require(__dirname + '/lib/init-routes.js');
-
-/* --- configuration    */
-var app = express();
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-
-/* --- pipeline         */
-app.use(initMongo.connect);
-app.use(initRoutes);
-app.use(morgan({format: 'dev'}));
-app.use(express.static(__dirname + '/static'));
-app.use('/less', less(__dirname + '/less'));
-app.use(bodyParser());
-app.use(methodOverride());
-app.use(cookieSession({keys:['SEC123', '321CES']}));
-
-/* --- http server      */
-var server = require('http').createServer(app);
-server.listen(port, function(){
-  console.log('Node server listening. Port: ' + port + ', Database: ' + dbname);
+http.createServer(app).listen(port, function(){
+  console.log('Node server listening. Port: ' + port + ', Directory: ' + directory);
 });
-
-/* --- socket.io        */
-var sockets = traceur.require(__dirname + '/lib/sockets.js');
-var io = require('socket.io').listen(server, {log:true, 'log level':2});
-io.of('/app').on('connection', sockets.connection);
-
-module.exports = app;
